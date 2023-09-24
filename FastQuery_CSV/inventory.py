b21d2cca481dc7cc63f859e076f2ad0591a5288a
jordan-horus <jordan.marques@ehorus.com.br>
2023-09-24 13:05:53 -0300

    Corrige verificação de -1 caso vazio e adiciona comentários

--- FastQuery_CSV/inventory.py
+++ FastQuery_CSV/inventory.py
@@ -1,101 +1,117 @@
 import re
 import csv
 
+# Lê o arquivo CSV e extrai as informações em 'header' e 'rows'
 with open('csv_and_dataproc/output_laptops.csv') as f:
     reader = csv.reader(f)
     rows = list(reader)
     header = rows[0]
     rows = rows[1:]
-    
+
+
+# Define a função 'row_price' para ser usada como chave na ordenação
 def row_price(row):
     return row[-1]
 
-class Inventory():                    
-    
+
+class Inventory:
+    # Inicializa a classe Inventory
     def __init__(self, csv_filename):
-        with open(csv_filename) as f: 
+        # Lê o arquivo CSV e carrega as informações
+        with open(csv_filename) as f:
             reader = csv.reader(f)
             rows = list(reader)
-        self.header = rows[0]        
+        self.header = rows[0]
         self.rows = rows[1:]
 
-        for row in self.rows:              
+        # Converte os preços para inteiros
+        for row in self.rows:
             row[-1] = int(row[-1])
-        
-        self.id_to_row = {}                        
-        for row in self.rows:                       
+
+        # Cria um dicionário para mapear IDs de laptops para linhas correspondentes
+        self.id_to_row = {}
+        for row in self.rows:
             self.id_to_row[row[0]] = row
-        
-        self.prices = set()                          
-        for row in self.rows:                        
+
+        # Cria um conjunto de preços únicos
+        self.prices = set()
+        for row in self.rows:
             self.prices.add(row[-1])
-        
+
+        # Ordena as linhas por preço usando a função 'row_price' como chave
         self.rows_by_price = sorted(self.rows, key=row_price)
-    
+
+    # Método para obter um laptop por ID (busca linear)
     def get_laptop_from_id(self, laptop_id):
-        for row in self.rows:                 
+        for row in self.rows:
             if row[0] == laptop_id:
                 return row
-        return None   
-    
-    def get_laptop_from_id_fast(self, laptop_id):  
-        if laptop_id in self.id_to_row:           
+        return None
+
+    # Método para obter um laptop por ID (busca rápida usando o dicionário 'id_to_row')
+    def get_laptop_from_id_fast(self, laptop_id):
+        if laptop_id in self.id_to_row:
             return self.id_to_row[laptop_id]
         return None
 
-    def check_promotion_dollars(self, dollars):    
-        for row in self.rows:                   
+    # Método para verificar promoção de laptops (busca linear)
+    def check_promotion_dollars(self, dollars):
+        for row in self.rows:
             if row[-1] == dollars:
                 return True
-        for row1 in self.rows:                  
+        for row1 in self.rows:
             for row2 in self.rows:
                 if row1[-1] + row2[-1] == dollars:
                     return True
-        return False                        
-    
+        return False
+
+    # Método para verificar promoção de laptops (busca rápida usando o conjunto 'prices')
     def check_promotion_dollars_fast(self, dollars):
-        if dollars in self.prices:                   
+        if dollars in self.prices:
             return True
-        for price in self.prices:                    
+        for price in self.prices:
             if dollars - price in self.prices:
                 return True
-        return False                                
-    
+        return False
+
+    # Método para encontrar um laptop por preço (busca binária)
     def find_laptop_with_price(self, target_price):
-        range_start = 0                                   
-        range_end = len(self.rows_by_price) - 1                       
+        range_start = 0
+        range_end = len(self.rows_by_price) - 1
         while range_start < range_end:
-            range_middle = (range_end + range_start) // 2  
+            range_middle = (range_end + range_start) // 2
             value = self.rows_by_price[range_middle][-1]
-            if value == target_price:                            
-                return range_middle                        
-            elif value < target_price:                           
-                range_start = range_middle + 1             
-            else:                                          
-                range_end = range_middle - 1 
-        if self.rows_by_price[range_start][-1] != target_price:                  
-            return -1                                      
+            if value == target_price:
+                return range_middle
+            elif value < target_price:
+                range_start = range_middle + 1
+            else:
+                range_end = range_middle - 1
+        if self.rows_by_price[range_start][-1] != target_price:
+            return -1
         return range_start
-    
-    def find_first_laptop_more_expensive(self, target_price): # Step 2
-        range_start = 0                                   
-        range_end = len(self.rows_by_price) - 1                   
+
+    # Método para encontrar o primeiro laptop mais caro (busca binária)
+    def find_first_laptop_more_expensive(self, target_price):
+        range_start = 0
+        range_end = len(self.rows_by_price) - 1
         while range_start < range_end:
-            range_middle = (range_end + range_start) // 2  
+            range_middle = (range_end + range_start) // 2
             price = self.rows_by_price[range_middle][-1]
             if price > target_price:
                 range_end = range_middle
             else:
                 range_start = range_middle + 1
-        if self.rows_by_price[range_start][-1] <= target_price:                  
-            return -1                                   
+        if self.rows_by_price[range_start][-1] <= target_price:
+            return -1
         return range_start
-    
+
+    # Método para encontrar o último laptop mais barato (busca binária)
     def find_last_laptop_cheaper(self, target_price):
         range_start = 0
         range_end = len(self.rows_by_price) - 1
         last_cheaper = -1  # Inicialmente, nenhum laptop mais barato é encontrado
-    
+
         while range_start <= range_end:
             range_middle = (range_end + range_start) // 2
             price = self.rows_by_price[range_middle][-1]
@@ -108,54 +124,69 @@ class Inventory():
 
         return last_cheaper
 
-    
+    # Método para encontrar laptops dentro de uma faixa de preços
+    def find_laptops_in_range(self, min_price, max_price):
+        if min_price > max_price:
+            return -1
+        min = self.find_first_laptop_more_expensive(min_price)
+        max = self.find_last_laptop_cheaper(max_price)
+        laptops_in_range = []
+        for i in range(min, max):
+            laptops_in_range.append(self.rows_by_price[i])
+        if len(laptops_in_range) == 0:
+            return -1
+        return laptops_in_range
+
     def find_laptops_in_range(self, min_price, max_price):
         if (min_price > max_price):
             return -1
         min = self.find_first_laptop_more_expensive(min_price)
         max = self.find_last_laptop_cheaper(max_price)
         laptops_in_range = []
-        for i in range(min,max):
+        for i in range(min, max):
             laptops_in_range.append(self.rows_by_price[i])
         if len(laptops_in_range) == 0:
             return -1
         return laptops_in_range
-    
+
+    # Método para encontrar o laptop mais barato com determinada RAM e memória
     def find_cheapest_laptop_with_ram_memory(self, target_ram, target_memory):
         for row in self.rows_by_price:
             if int(row[7]) == target_ram and int(row[8]) == target_memory:
                 return row
-    
+
         return -1  # Retorna -1 se nenhum laptop for encontrado
-    
-    def find_cheapest_specs(self, ram_text,mem_text):
+
+    # Método para encontrar laptops com as especificações mais baratas
+    def find_cheapest_specs(self, ram_text, mem_text):
+        # Extrai o valor e a unidade da RAM a partir do texto (exemplo: "8 GB")
         match = re.search(r'(\d+)\s*(TB|GB)', ram_text, re.IGNORECASE)
         if match:
             value = int(match.group(1))
             unit = match.group(2).lower()
-        
+
             # Converte a unidade para GB, se necessário
             if unit == "tb":
                 value *= 1000
 
             ram = value
         else:
-            return -1
+            return -1  # Retorna -1 se não for possível analisar a RAM
+
+        # Extrai o valor e a unidade da memória a partir do texto (exemplo: "256GB")
         match = re.search(r'(\d+)\s*(TB|GB)', mem_text, re.IGNORECASE)
         if match:
             value = int(match.group(1))
             unit = match.group(2).lower()
-        
+
             # Converte a unidade para GB, se necessário
             if unit == "tb":
                 value *= 1000
 
             memory = value
         else:
-            return -1
-        
-        return Inventory.find_cheapest_laptop_with_ram_memory(self,ram,memory)
+            return -1  # Retorna -1 se não for possível analisar a memória
 
-        
-        
-    
+        # Chama o método 'find_cheapest_laptop_with_ram_memory' com os valores extraídos
+        return Inventory.find_cheapest_laptop_with_ram_memory(self, ram, memory)
+
--- FastQuery_CSV/main.py
+++ FastQuery_CSV/main.py
@@ -3,25 +3,29 @@ from inventory import Inventory
 inventory = Inventory('csv_and_dataproc/output_laptops.csv')
 
 start = input("Deseja buscar algum modelo (S ou N)? ")
-if start == "N":
+if start == "N" or start == "n":
     print("Certo!")
-while start == "S":
+while start == "S" or start == "s":
     p1 = input("Como deseja buscar? \n1 - Por faixa de preço (em euros); \n2 - O mais barato por RAM e memória\nR: ")
     if p1 == "1":
-        minimo = input("Nestes campos digite apenas o valor\nDigite o menor valor desejado (€): ")
+        minimo = input("Nestes campos digite apenas o valor numérico\nDigite o menor valor desejado (€): ")
         maximo = input("Digite o maior valor desejado (€): ")
-        laptops = inventory.find_laptops_in_range(int(minimo),int(maximo))
+        laptops = inventory.find_laptops_in_range(int(minimo), int(maximo))
         if laptops == -1:
             print("Não há aparelhos nessa faixa de valor.")
         else:
+            print("Produtos encontrados: ")
             for line in laptops:
                 linha = ' '.join(map(str, line))  # Converte os elementos em strings e junta-os com espaço
-                print(linha)
+                print(f"LAPTOP: {linha}")
     if p1 == "2":
         ram = input("Lembre de colocar a unidade (em GB ou TB)\nQual a quantidade de RAM desejado: ")
         memory = input("Qual a quantidade de Memória desejada: ")
-        laptop = inventory.find_cheapest_specs(ram,memory)
-        print(laptop)
+        laptop = inventory.find_cheapest_specs(ram, memory)
+        if laptop == -1:
+            print("Não há aparelhos com essas especificações.")
+        else:
+            print(laptop)
     start = input("Deseja buscar outro modelo (S ou N)? ")
     
 
